8ccb3df0841a150b862ab7948518581b35ef3c19
Galia <galia.musllimova@gmail.com>
2020-07-29 15:46:16 +0500

    Updates

---
+++ precache-manifest.9d33c4dd1323838c060287ed3cac2cf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce64a586f16287f65428138a5509011a",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/index.html"
+  },
+  {
+    "revision": "522ce3e5d6fb84a75351",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "756aa3a370a193d164cc",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "522ce3e5d6fb84a75351",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/js/2.2ba34d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/js/2.2ba34d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756aa3a370a193d164cc",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/js/main.cc89a6cf.chunk.js"
+  },
+  {
+    "revision": "20201363a405e34125d0",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/js/runtime-main.92ae5297.js"
+  },
+  {
+    "revision": "98491cb4556832d78e073b887376985a",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/media/cloud.98491cb4.png"
+  },
+  {
+    "revision": "66d07c0d631505894474902a30895b67",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/media/clouds-sun.66d07c0d.png"
+  },
+  {
+    "revision": "206174858450b5f090b5b7edd74946ab",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/media/rain.20617485.png"
+  },
+  {
+    "revision": "3260a1dc26617a5f452a4ae0d4042f41",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/media/snow.3260a1dc.png"
+  },
+  {
+    "revision": "c7171d58b925264cdd69cfc6503de738",
+    "url": "/galiaMuslimova/weatherApp/tree/gh-pages/static/media/sun.c7171d58.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/galiaMuslimova/weatherApp/precache-manifest.070854cf232409665b7b6529405f4073.js"
+  "/galiaMuslimova/weatherApp/tree/gh-pages/precache-manifest.9d33c4dd1323838c060287ed3cac2cf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/galiaMuslimova/weatherApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/galiaMuslimova/weatherApp/tree/gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
